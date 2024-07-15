ba1ca8e82f86e9fe239933bb9d3ee73ec4186596
psvsai <pasupuletisvsm245@gmail.com>
2024-07-15 19:18:22 +0530

    psvsai

---
+++ mondayassignment/src/DrivingLicense.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class DrivingLicense {
+    public static void main(String[] args) throws Exception {
+        Scanner scan = new Scanner(System.in);
+        
+        int id = scan.nextInt();
+        String name = scan.nextLine();
+        int age = scan.nextInt();
+        String address = scan.nextLine();
+
+        System.out.println("int id" );
+        
+        System.out.println("name of the person");
+
+        System.out.println("age of the person");
+        
+        System.out.println("address");
+       
+       
+       
+       
+       
+    }
+}

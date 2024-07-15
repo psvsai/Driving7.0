f76f1b727dddb82e6095798c310d2083c02b0d9e
psvsai <pasupuletisvsm245@gmail.com>
2024-07-15 22:46:04 +0530

    hi

--- DrivingLicense.java
+++ DrivingLicense.java
@@ -2,11 +2,27 @@ import java.util.Scanner;
 
 public class DrivingLicense {
     public static void main(String[] args) {
-        Scanner in=new Scanner(System.in);
-        int id;
-        String name;
-        int age;
-        String address;
-// Remaining Code Here
+        Scanner scan = new Scanner(System.in);
+        
+        int id = scan.nextInt();
+        String name = scan.next();
+        int age = scan.nextInt();
+        String address = scan.next();
+
+        System.out.println("license id " + id );
+        
+        System.out.println("name of the person " + name);
+
+        System.out.println("age of the person " + age);
+        
+        System.out.println("address " + address);
+        if (age < 18)
+        {
+            System.out.println("Not eligble for DRIVING LICENSE");
+        }
+        else 
+        {
+            System.out.println("Eligble for DRIVING LICENSE");
+        }
     }
 }
